e2a3b540c5b529c1b867290333c953ec7c973e2b
Henrique <henrique.moriggi@hotmail.com>
2022-01-17 11:52:11 -0300

    comando delete

--- index.php
+++ index.php
@@ -42,10 +42,20 @@
     echo $aluno;
     */
 
+    /*
+    Alterar um usuário
     $usuario = new Usuario();
     $usuario->loadById(8);
     $usuario->update("professor", "!@#$%¨&*");
     echo $usuario;
-    
+    */
+
+    /*
+    Excluindo um usuário
+     */
+    $usuario = new Usuario();
+    $usuario->loadById(7);
+    $usuario->delete();
+    echo $usuario;
 
 ?>
